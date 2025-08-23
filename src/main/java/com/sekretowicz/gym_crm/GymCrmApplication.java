c84c7cd059546a47a7cf9cbab93a375401a1937d
Sekretowicz <83813277+Sekretowicz@users.noreply.github.com>
2025-08-23 18:30:57 +0300

    SQS support instead of JMS.

--- src/main/java/com/sekretowicz/gym_crm/GymCrmApplication.java
+++ src/main/java/com/sekretowicz/gym_crm/GymCrmApplication.java
@@ -2,10 +2,8 @@ package com.sekretowicz.gym_crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
-@EnableJms
 public class GymCrmApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/sekretowicz/gym_crm/messaging/producer/WorkloadPublisher.java
+++ src/main/java/com/sekretowicz/gym_crm/messaging/producer/WorkloadPublisher.java
@@ -1,18 +1,19 @@
 package com.sekretowicz.gym_crm.messaging.producer;
 
-import com.sekretowicz.gym_crm.messaging.config.JmsConfig;
 import com.sekretowicz.gym_crm.messaging.dto.WorkloadMessageDto;
+import io.awspring.cloud.sqs.operations.SqsTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WorkloadPublisher {
 
+    //Previously it was JmsTemplate
     @Autowired
-    private JmsTemplate jmsTemplate;
+    private SqsTemplate sqsTemplate;
 
     public void publish(WorkloadMessageDto dto) {
-        jmsTemplate.convertAndSend(JmsConfig.WORKLOAD_QUEUE, dto);
+        //sqsTemplate.convertAndSend(JmsConfig.WORKLOAD_QUEUE, dto);
+        sqsTemplate.send("gym-crm-queue", dto);
     }
 }
